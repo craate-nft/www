47bae7525e4fb5fe1376fc3cb4a3b9a334cb00f3
Shaba Abhiram <shabarivas.abhiram@gmail.com>
2022-03-09 17:22:31 -0800

    www

---
+++ precache-manifest.e11fe9f9b37852b924930250226902da.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b877ea76b667a236ff0f191f01a7220f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e80f128328ce3acb2e31",
+    "url": "/static/css/main.f4ea80bc.chunk.css"
+  },
+  {
+    "revision": "ae13585085c456174f83",
+    "url": "/static/js/2.90d42ecf.chunk.js"
+  },
+  {
+    "revision": "1ec54c518ced73552a48c25815983d38",
+    "url": "/static/js/2.90d42ecf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e80f128328ce3acb2e31",
+    "url": "/static/js/main.2549b636.chunk.js"
+  },
+  {
+    "revision": "1d503352208bdaf09093",
+    "url": "/static/js/runtime-main.084c49b5.js"
+  },
+  {
+    "revision": "fcafa6817187fc505d1bb22ddcad8261",
+    "url": "/static/media/buy.fcafa681.svg"
+  },
+  {
+    "revision": "25b810517d6491efdbf7f935dcb306a7",
+    "url": "/static/media/collect.25b81051.svg"
+  },
+  {
+    "revision": "e22175220ed9b9e88162d8efe3bdc17a",
+    "url": "/static/media/discord.e2217522.svg"
+  },
+  {
+    "revision": "3114588928f0f0cb64022ac062e56cf2",
+    "url": "/static/media/github.31145889.svg"
+  },
+  {
+    "revision": "91c20fca35b90da3514e4d1e8fd6a7f7",
+    "url": "/static/media/opensea-logo.91c20fca.svg"
+  },
+  {
+    "revision": "79cd76aec1148d2461346ec30c648df9",
+    "url": "/static/media/puzzle.79cd76ae.svg"
+  },
+  {
+    "revision": "1dee27c4f51ec4223324e8b4459b663f",
+    "url": "/static/media/reddit.1dee27c4.svg"
+  },
+  {
+    "revision": "2db009d884b526dbd3ccd985cb52de25",
+    "url": "/static/media/regular.2db009d8.ttf"
+  },
+  {
+    "revision": "c4c37578ba2bb90be6b42b9f2ca92b23",
+    "url": "/static/media/salary.c4c37578.svg"
+  },
+  {
+    "revision": "3ad6654a2a73811c6e91479ff9015dfb",
+    "url": "/static/media/twitter.3ad6654a.svg"
+  }
+]);
